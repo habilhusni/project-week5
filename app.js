a964d8f3ca0819c1c111bfe2f81b743a36b12e67
arfanizar <arfanizar.fth@gmail.com>
2017-03-30 18:47:02 +0700

    arfanizar first release

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyD5h9OsBiep0Gm_ijKxjuVZ6I89XSSagJg'
+});
+var speech = require('@google-cloud/speech')({
+  projectId: 'black-anagram-163109',
+  keyFilename: './keyfile.json'
+});
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/', index);
-
 app.listen(3000)
+
+app.use('/',function (req,res,next) {
+  googleMapsClient.geocode({
+    address: 'jalan Setiabudi no.200 bandung'
+  }, function(err, response) {
+    if (!err) {
+      res.send(response.json.results);
+    }
+  });
+});
+
+app.use('/sound',function (req,res,next) {
+// Detect the speech in an audio file.
+speech.recognize('./audio.raw', {
+  encoding: 'LINEAR16',
+  sampleRate: 16000
+}, function(err, transcript) {
+  res.send(transcript)
+  // transcript = 'how old is the Brooklyn Bridge'
+});
+
+});
 
 module.exports = app
