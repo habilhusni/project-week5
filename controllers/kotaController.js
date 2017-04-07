6f3fcd52f363813973de5810bd7320db6cdf47d5
habilhusni <habilhusni@gmail.com>
2017-04-07 15:53:28 +0700

    habilhusni-modified-kotaTesting

--- controllers/kotaController.js
+++ controllers/kotaController.js
@@ -16,36 +16,44 @@ let getAll = function (req, res, next) {
 let createOne = function (req, res, next) {
   Kota.create({
     kota_id: req.body.kota_id,
-    kota_name: req.body.kota_name
+    kota_name: req.body.kota_name,
+    img: req.body.img
   }, function (error, user){
-    if(error) throw error;
-    res.send(user);
+    if(error) res.send(error);
+    else {
+      res.send(user);  
+    }
   })
 };
 let update = function (req, res, next) {
   Kota.findOne({kota_id: req.params.kota_id}, function (err, kota) {
-  if (err) return handleError(err);
-
-  kota.kota_name = req.body.kota_name;
-  kota.save(function (err, updatedKota) {
-    if (err) return handleError(err);
-    res.send(updatedKota);
-  });
+    if (err) res.send(err);
+    else if(!kota) res.send({errors: 'Kota not found'})
+    else {
+      if(req.body.kota_id) kota.kota_id = req.body.kota_id;
+      if(req.body.kota_name) kota.kota_name = req.body.kota_name;
+      if(req.body.img) kota.img = req.body.img;
+      kota.save(function (err, updatedUser) {
+        if (err) res.send(err);
+        res.send(updatedUser);
+      });
+    }
   });
 };
 let deleteOne = function (req, res, next) {
-  Kota.findOne({kota_id: req.params.kota_id}).remove(function(err){
-    res.send(err);
+  Kota.findOne({kota_id: req.params.kota_id}).remove(function(err, respond){
+    if (err) res.send(err);
+    res.send(respond);
   })
 };
 let find = function (req,res,next) {
   Kota.findOne({kota_name: req.query.input},function(err,kota){
-    if(err) return handleError(err);
+    if(err) res.send(err);
     if(kota){
       Wisata.find({kota:kota._id})
       .populate('kota')
       .exec(function(err, wisata){
-        if(err) return handleError(err);
+        if(err) res.send(err);
         mapping(wisata)
         .then(function(wisata){
           res.send(wisata)
